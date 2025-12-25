de8fcad6bd9003139e191fc12537cc53699756d1
Safayet2004 <safayet.ksoft@gmail.com>
2025-12-25 10:42:16 +0600

    Prepare static files

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 from decouple import config
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,7 +32,7 @@ DEBUG = config('DJANGO_DEBUG', cast=bool)
 print("DEBUG = ", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
-    ".railway.app" #https://saas.prod.railway.app
+    ".railway.app"
 ]
 
 if DEBUG:
@@ -141,6 +143,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / 'vendors'
+
+# Sources for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+# Output for python manage.py collectstatic
+# local cdn
+STATIC_ROOT = BASE_DIR.parent / "local-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
